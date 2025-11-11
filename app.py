4ee175f9ddbff42ee9ba9fffe233c260c479de17
Tuhin <145905935+tuheen27@users.noreply.github.com>
2025-11-11 20:23:52 +0530

    add new section

--- app.py
+++ app.py
@@ -5,6 +5,8 @@ app = Flask(__name__)
 @app.route('/')
 def home():
     return "Hello from Flask app running via Jenkins CI/CD!"
-
+@app.route('/status')
+def status():
+    return "Application is running smoothly!"
 if __name__ == '__main__':
     app.run(host='0.0.0.0', port=5000)
